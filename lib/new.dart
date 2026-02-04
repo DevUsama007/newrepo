a6f9bdfab98dbef3111b2e5e6b2dd8121cf40a29
DevUsama007 <uuusama0026@gmail.com>
2026-02-04 10:56:50 +0500

    hi2

---
+++ lib/new.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewHi extends StatefulWidget {
+  const NewHi({super.key});
+
+  @override
+  State<NewHi> createState() => _NewHiState();
+}
+
+class _NewHiState extends State<NewHi> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
